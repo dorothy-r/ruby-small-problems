b31ed210338099cdf70c72311f525eaee2af3e6b
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2022-01-05 13:46:31 -0500

    Medium 2.1

---
+++ medium2/longest_sentence.rb
@@ -0,0 +1,46 @@
+=begin
+# Problem: Write a program that reads the content of a text file and then prints
+the longest sentence in the file based on number of words. Sentences may end
+with perios, exclamation points, or question marks. Any sequence of characters
+that are not spaces or sentence-ending characters should be treated as a word.
+You should also print the number of words in the longest sentence.
+
+# Example: 
+Input is a section of the Gettysburg address.
+Output: It is rather for us to be here dedicated to the great task remaining
+before us -- that from these honored dead we take increased devotion to that 
+cause for which they gave the last full measure of devotion -- that we here 
+highly resolve that these dead shall not have died in vain -- that this nation, 
+under God, shall have a new birthof freedom -- and that government of the 
+people, by the people, for the people, shall not perish from the earth.
+86 words.
+
+# Data Structures:
+Input: txt file
+Should split it into an array of sentences.
+And then further split each sentence into an array of words?
+Output: Print a string (the longest sentence and its word count).
+
+# Algorithm:
+Read the text file.
+Split it according to sentence-ending characters.
+Iterate over the resulting array of sentences, splitting those into words,
+and counting the words.
+Find the longest sentence. 
+Print it to the screen, as well as the number of words it contains.
+=end
+
+def find_longest(txt_file)
+  file = File.open(txt_file)
+  text = file.read
+  sentences = text.split(/\.|\?|!/)
+  word_counts = sentences.map { |sentence| sentence.split.size }
+  longest_number = word_counts.max
+  longest_index = word_counts.index(longest_number)
+  longest_sentence = sentences[longest_index]
+  puts "The longest sentence is: "
+  puts longest_sentence
+  puts "It has #{longest_number} words."
+end
+
+find_longest("frankenstein.txt")
